69c5151a6142aecb85171bf35d47a69561162d89
Eduard <sorokolietov@gmail.com>
2023-11-26 22:33:37 +0100

    height of contactList dynamic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Col, Container, Row } from 'react-bootstrap';
 
-// ========== QUESTIONS ==========
+// ==================== QUESTIONS ====================
 
-//  1. чи потрібно тут робити селектор для рендеру ContactList
-//     якщо витягувати селектор чи іде повторний реквест на бекенд?
-//     після запиту повертаєтсья contacts пустий масив
+//  1. У випадку коли на бекенді немає даних, ми не хочемо рендерити пусту розмітку.
+//     Задля перевірки чи масив контактів не пустий я витягую ці данні через useSelector.
+//     Мабуть функція асинхронна і тому в цей момент в змінну contacts записуєтся пустий
+//     масив.
+//     traceback: розкоментувати позначений стрілками код і перегрузити сторінку в
+//     браезері
 
 export const App = () => {
   // const contacts = useSelector(state => state.contacts.items);
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react';
 import { ContactListItem } from './ContactListItem';
 
 export const ContactList = () => {
+  const [contactListHeight, setContactListHeight] = useState(null);
+  console.log('contactListHeight: ', contactListHeight);
+
+  useEffect(() => {
+    setContactListHeight(window.innerHeight - 214);
+  }, []);
+
   return (
     <ul
       style={{
         padding: 0,
         marginTop: 20,
-        height: 480,
+        height: contactListHeight,
         overflow: 'hidden',
         overflowY: 'scroll',
       }}
